1706e6bd8b1ef68418d4ebaede58243f2986aced
Checksum <24845326+ChecksumDev@users.noreply.github.com>
2022-02-12 19:06:50 -0600

    It's `GameVersion` now.
    Took 26 seconds

--- Kryptonite/Utils/DownloadClient.cs
+++ Kryptonite/Utils/DownloadClient.cs
@@ -1,27 +1,23 @@
 ﻿using System.Diagnostics;
 using System.Net;
 using Kryptonite.Types;
-using Version = Kryptonite.Types.Version;
 
 namespace Kryptonite.Utils;
 
 public static class DownloadClient
 {
-    public static void Download(Instance instance, Version version, User user)
+    public static void Download(Instance instance, GameVersion version, User user)
     {
-        var beatSaberPath =
-            $"{Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)}\\Kryptonite\\Instances\\{instance.name}\\Beat Saber";
+        var beatSaberPath = $"{Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)}\\Kryptonite\\Instances\\{instance.Name}\\Beat Saber";
 
         try
         {
-            Terminal.Log($"Downloading Beat Saber v{version.version}");
-            var download = Process.Start("dotnet",
-                $"DepotDownloader\\DepotDownloader.dll -app 620980 -depot 620981 -manifest {version.manifest} -username {user.name} -password {new NetworkCredential(string.Empty, user.password).Password} -dir \"{beatSaberPath}\" -validate");
-
+            Terminal.Log($"Downloading Beat Saber v{version.Version} for {instance.Name}...");
+            using var download = Process.Start("dotnet", $"DepotDownloader\\DepotDownloader.dll -app 620980 -depot 620981 -manifest {version.Manifest} -username {user.Name} -password {new NetworkCredential(string.Empty, user.Password).Password} -dir \"{beatSaberPath}\" -validate");
             download.WaitForExit();
             Terminal.Log(
                 download.ExitCode == 0
-                    ? $"Successfully downloaded Beat Saber v{version.version}\nPress any key to return to the main menu."
+                    ? $"Successfully downloaded Beat Saber v{version.Version}\nPress any key to return to the main menu."
                     : $"Error: Received exit code {download.ExitCode} from Depot Downloader.\nPress any key to return to the main menu.",
                 hold: true);
         }
