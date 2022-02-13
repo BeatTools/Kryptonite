1d4f73c073aa2c280db7d0240e4b2563ea08d389
Checksum <24845326+ChecksumDev@users.noreply.github.com>
2022-02-12 19:08:39 -0600

    Completely rework some `Database.cs` methods to use Instance classes; This should be the norm and will be refactored in the future.
    Took 1 minute

--- Kryptonite/Utils/Database.cs
+++ Kryptonite/Utils/Database.cs
@@ -1,7 +1,7 @@
 ﻿using System.Data;
 using System.Data.SQLite;
+using System.Net;
 using Kryptonite.Types;
-using Version = Kryptonite.Types.Version;
 
 namespace Kryptonite.Utils;
 
@@ -44,7 +44,9 @@ internal static class Database
 
     public static async void Initialize()
     {
+        Terminal.Log("Dropping versions table...");
         ExecuteWrite("drop table if exists versions");
+        Terminal.Log("Creating instances table...");
         ExecuteWrite(@"create table if not exists instances
         (
             name      TEXT not null,
@@ -52,6 +54,7 @@ internal static class Database
             _default integer default 0 not null
         );");
 
+        Terminal.Log("Creating versions table...");
         ExecuteWrite(@"create table if not exists versions
         (
             version  TEXT not null
@@ -59,45 +62,38 @@ internal static class Database
                     primary key,
             manifest TEXT not null
         );");
-
+        
+        Terminal.Log("Getting versions...");
         using var client = new HttpClient();
-        // Download the SQL for the versions table and execute it (This is to ensure that the versions table is always up to date)
-        var versions =
-            await client.GetAsync(
-                "https://gist.githubusercontent.com/ChecksumDev/ca69ccd781e37f3e5a2afe9e2bb1ed69/raw/270eed764c897ba46d4db17961b05910db090a3f/beatsaber_versions.sql");
+        using var versions = await client.GetAsync("https://gist.githubusercontent.com/ChecksumDev/ca69ccd781e37f3e5a2afe9e2bb1ed69/raw/270eed764c897ba46d4db17961b05910db090a3f/beatsaber_versions.sql");
         var versionsSql = await versions.Content.ReadAsStringAsync();
 
-        if (versionsSql.StartsWith("INSERT INTO"))
+        if (versions.StatusCode == HttpStatusCode.OK && versionsSql.StartsWith("INSERT INTO"))
             ExecuteWrite(versionsSql);
         else
-            throw new Exception("Failed to download versions table");
+            throw new Exception("Failed to download versions table from GitHub.");
     }
 
-    public static List<Version>? ListVersions()
+    public static List<GameVersion> ListVersions()
     {
         const string query = "SELECT * FROM versions";
 
         var dataTable = Execute(query);
-        if (dataTable == null || dataTable.Rows.Count == 0) return null;
-
+        if (dataTable == null) throw new Exception("Failed to get versions.");
         return (from DataRow dataRow in dataTable.Rows
-            select new Version(dataRow["version"].ToString()!, dataRow["manifest"].ToString()!)).ToList();
+            select new GameVersion(dataRow["version"].ToString(), dataRow["manifest"].ToString())).ToList();
     }
 
-    public static Version? GetVersion(string version)
+    public static GameVersion? GetVersion(string version)
     {
         const string query = "SELECT * FROM versions WHERE version = $version";
 
-        var args = new Dictionary<string, object>
-        {
-            {"$version", version}
-        };
-
+        var args = new Dictionary<string, object> {{"$version", version}};
         var dataTable = Execute(query, args);
 
         if (dataTable == null || dataTable.Rows.Count == 0) return null;
 
-        return new Version(dataTable.Rows[0].Field<string>("version")!,
+        return new GameVersion(dataTable.Rows[0].Field<string>("version")!,
             dataTable.Rows[0].Field<string>("manifest")!);
     }
 
@@ -108,21 +104,17 @@ internal static class Database
 
         var dataTable = Execute(query);
         if (dataTable == null || dataTable.Rows.Count == 0) return null;
-
-        return dataTable.AsEnumerable()
-            .Select(row => new Instance(row.Field<string>("name"), row.Field<string>("version")))
-            .ToList();
+        
+        return (from DataRow dataRow in dataTable.Rows
+            select new Instance(dataRow["name"].ToString()!, dataRow["version"].ToString()!)).ToList();
     }
 
     public static Instance CreateInstance(string name, string version)
     {
         const string query = "insert into instances (name, version) values ($name, $version);";
 
-        var args = new Dictionary<string, object>
-        {
-            {"$name", name}, {"$version", version}
-        };
-
+        var args = new Dictionary<string, object> {{"$name", name}, {"$version", version}};
+        
         var result = ExecuteWrite(query, args);
         if (result == 0) throw new Exception("Failed to create instance");
 
@@ -133,10 +125,7 @@ internal static class Database
     {
         const string query = "SELECT name, version FROM instances WHERE name = $name";
 
-        var args = new Dictionary<string, object>
-        {
-            {"$name", name}
-        };
+        var args = new Dictionary<string, object> {{"$name", name}};
 
         var dataTable = Execute(query, args);
 
@@ -162,7 +151,7 @@ internal static class Database
         return result;
     }
 
-    public static int SetDefaultInstance(string name)
+    public static int SetDefaultInstance(Instance instance)
     {
         // There can only be one default instance, so we need to unset the current default if there is one
         const string query = "UPDATE instances SET _default = 0 WHERE _default = 1";
@@ -170,7 +159,7 @@ internal static class Database
 
         // Set the new default
         const string query2 = "UPDATE instances SET _default = 1 WHERE name = $name";
-        var args = new Dictionary<string, object> {{"$name", name}};
+        var args = new Dictionary<string, object> {{"$name", instance.Name}};
 
         return ExecuteWrite(query2, args);
     }
