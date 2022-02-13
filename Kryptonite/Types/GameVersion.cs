199999be9b40502e192a8cc6799b6a642d156b75
Checksum <24845326+ChecksumDev@users.noreply.github.com>
2022-02-12 19:03:06 -0600

    Rename `Version.cs` to `GameVersion.cs` to reduce workarounds.
    Took 30 seconds

---
+++ Kryptonite/Types/GameVersion.cs
@@ -0,0 +1,13 @@
+﻿namespace Kryptonite.Types;
+
+public class GameVersion
+{
+    public GameVersion(string version, string manifest)
+    {
+        Version = version;
+        Manifest = manifest;
+    }
+
+    public string Version { get; }
+    public string Manifest { get; }
+}
